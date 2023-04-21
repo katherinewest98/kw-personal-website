3258638b154d65726a14894b6b323a6e9722b3fd
Katherine West <katherinewest98@yahoo.com>
2023-04-21 05:33:35 -0500

    Update JS event listener

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function surpriseMessage() {
-    alert("Oops - surprise not found! Please come back when Katherine has completed the Flatiron School boot camp to see the surprise! :-)")
-}
+const button = document.querySelector("button");
 
-let surpriseButton = document.querySelector("button");
+const canvas = document.querySelector("canvas");
 
-surpriseButton.addEventListener("click", surpriseMessage);
+const jsConfetti = new JSConfetti();
+
+button.addEventListener("click", () => {
+    jsConfetti.addConfetti({
+        confettiColors: [
+          '#FE8F40', '#8881E0', '#FE5E71', '#5B9D2B', '#01A1FE', '#F46DBF',
+        ],
+      })
+});
