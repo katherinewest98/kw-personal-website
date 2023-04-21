02cef96589a8959f3567a8d3e6c5b6593ce68c06
Katherine West <katherinewest98@yahoo.com>
2023-04-21 03:56:51 -0500

    Add image, update CSS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function surpriseMessage() {
-    alert("404: Surprise not found. Please come back when Katherine has completed the Flatiron School boot camp to see the surprise!")
+    alert("Oops - surprise not found! Please come back when Katherine has completed the Flatiron School boot camp to see the surprise! :-)")
 }
 
 let surpriseButton = document.querySelector("button");
